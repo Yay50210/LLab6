2429f8f7dbd216698b074b352a931122b6b5a95c
Best <Photchara_k@cmu.ac.th>
2023-12-01 16:53:47 +0700

    "lab6.cpp completed"

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+
+int N;
+int E = 0;
+int O = 0;
+while(N!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+    cin >> N;
+    if(N%2==0){
+        E+=1;}
+    
+    
+        else{
+        O+=1;
+    }
+
+}
+    cout << "#Even numbers = "<<E-1<<"\n";
+    cout << "#Odd numbers = "<<O;
+
+
+   return 0;
+
 }
--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+double deg2rad(double x) {
+    return ((M_PI*x)/180);
+}
+double rad2deg(double y) {
+    return y*((180)/(M_PI));
+}
+double findXComponent(double l1, double l2, double a1, double a2) {
+    return l1*cos(a1)+l2*cos(a2);
+}
+double findYComponent(double l1, double l2, double a1, double a2) {
+    return l1*sin(a1)+l2*sin(a2);
+}
+double  pythagoras(double x, double y) {
+    double z;
+    z = pow(x,2)+pow(y,2);
+    return pow(z,0.5);
+}
+void showResult(double b, double a) {
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+    cout << "\nLength of the resultant vector = " << b;
+    cout << "\nDirection of the resultant vector (deg) = " << a;
+    cout << "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+}
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
